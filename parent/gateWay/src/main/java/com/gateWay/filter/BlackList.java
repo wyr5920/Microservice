b3f990a1ffc564eca38de04d2ea52fc73676e74c
wyr5920 <1501194238@qq.com>
2022-07-20 11:30:41 +0800

    网关配置

---
+++ parent/gateWay/src/main/java/com/gateWay/filter/BlackList.java
@@ -0,0 +1,63 @@
+package com.gateWay.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 定义全局过滤器，对所有的路由生效；黑白名单例子
+ */
+@Slf4j
+@Component
+public class BlackList implements GlobalFilter, Ordered {
+
+    //数据可来自redis或者数据库
+    private static List<String> blackList = new ArrayList<>();
+
+    /**
+     * 过滤器核心方法
+     * @param exchange 封装了request和response对象的上下文
+     * @param chain 网关过滤器链(包含全局过滤器和单路由过滤器) * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 思路:获取客户端ip，判断是否在黑名单中，在的话就拒绝访问，不在的话 就放行
+        // 从上下文中取出request和response对象
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        // 从request对象中获取客户端ip
+        String clientIp = request.getRemoteAddress().getHostString();
+        if (blackList.contains(clientIp)){
+            //拒绝访问
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);//状态码
+            log.debug("=====>IP:" + clientIp + " 在黑名单中，将被拒绝访问");
+            String data = "Request be denied!";
+            // 拿着clientIp去黑名单中查询，存在的话就决绝访问
+            DataBuffer wrap = response.bufferFactory().wrap(data.getBytes());
+            return response.writeWith(Mono.just(wrap));
+        }
+        //放行
+        return chain.filter(exchange);
+    }
+
+
+    /**
+     * 返回值表示当前过滤器的顺序(优先级)，数值越小，优先级越高 * @return
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
