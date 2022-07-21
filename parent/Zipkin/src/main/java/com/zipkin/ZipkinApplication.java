0a4f1156a56d09750874488fd214d963c32499ec
wyr5920 <1501194238@qq.com>
2022-07-21 17:06:19 +0800

    链路追踪配置

---
+++ parent/Zipkin/src/main/java/com/zipkin/ZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer // 开启Zipkin Server功能
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class,args);
+    }
+}
